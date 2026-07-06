b008d0ac0a17952d58c174aff533d2a63ffd3642
yash <yashraj.ch2048@gmail.com>
2026-07-06 12:26:08 +0530

    Add buttons for permissions and toggling feed on and off

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,33 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-
+  let playing = $state(false);
+  let hasPerms = $state(false);
   let stream: MediaStream | null = null;
   let myVideo: HTMLMediaElement | null;
-  const constraints = { video: true };
+  const constraints = { video: true, audio: true };
 
   const askPermissions = async () => {
     try {
       stream = await navigator.mediaDevices.getUserMedia(constraints);
+      if (stream) {
+        hasPerms = true;
+      }
     } catch (error) {}
   };
 
   const toggleFeed = () => {
-    myVideo = document.querySelector("#incoming");
     console.log("toggleFeed is working");
-    if (myVideo) {
-      myVideo.srcObject = stream;
-    }
-  };
-  const startVideo = async () => {
-    let astream: MediaStream | null = null;
-
-    try {
-      astream = await navigator.mediaDevices.getUserMedia(constraints);
-      myVideo = document.querySelector("#incoming");
-      console.log("startVideo is working");
-      console.log(myVideo);
-      console.log(astream);
-      
+    myVideo = document.querySelector("#incoming");
+    if (playing) {
+      const tracks = stream?.getTracks();
+      tracks?.forEach((track) => track.stop());
+      if (myVideo) myVideo.srcObject = null;
+      hasPerms = false;
+    } else {
       if (myVideo) {
-        myVideo.srcObject = astream;
+        myVideo.srcObject = stream;
       }
-    } catch (error) {}
+    }
+    playing = !playing;
   };
   // state
 </script>
@@ -39,8 +35,9 @@
 <main>
   <section class="options">
     <button onclick={askPermissions}>Get Permissions</button>
-    <button onclick={toggleFeed}>Show my video</button>
-    <button onclick={startVideo}>Start Video</button>
+    <button onclick={toggleFeed} disabled={!hasPerms}
+      >{playing ? "Stop" : "Show"} my video</button
+    >
   </section>
   <section class="feed">
     <video id="incoming" autoplay></video>
@@ -51,5 +48,20 @@
 <style>
   video {
     background-color: cornflowerblue;
+  }
+  main {
+    display: flex;
+    padding: 5rem;
+    justify-content: space-between;
+  }
+  .options,
+  .feed {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  button:nth-child(1) {
+    background-color: #005f3d;
+    border: 0;
   }
 </style>
